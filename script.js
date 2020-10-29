b9fcd3af9eb997b6842b0dcd5948e962be8179e1
Mary Papamichalis <mary.papamichalis@gmail.com>
2020-10-29 00:21:35 -0500

    final

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
 var startButton = document.getElementById("start-btn");
-var questionsEl = document.querySelector("#questions");
+var questions = document.querySelector("#questions");
 var choicesEl = document.getElementById("choices");
 var timerEl = document.getElementById("timer");
+var choices = Array.from(document.getElementsByClassName("choice-text"));
 
-// var questions =[
-//     {
-//         question: "What is?"
-//         choices:
-//     }
-// ]
+ var questions =[
+    {
+        question: "Inside which HTML element do we put the JavaScript??",
+        choices: ["<script>", "<javascript>", "<js>", "<scripting>"],
+        answer: [0],      
+    },
+        
+    {
+        question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
+        choices: ["<script href='xxx.js'>", "<script name='xxx.js'>", "<script src='xxx.js'>", "<script file='xxx.js'>"],
+        answer: [2],
+    },
+    
+    {
+        question: "How do you write 'Hello World' in an alert box?",
+        choices: ["msgBox('Hello World');", "alertBox('Hello World');", "msg('Hello World');", "alert('Hello World');"],
+        answer: [3],
+    },
+        
+ ];
 
 
-// var timer = 100;
-//     var intervalID = setInterval(function(){
-//         time--
-//         console.log(time)
-//     }, 1000)
-// clearInterval(intervalID)
-// time +=10;
-// time -=10;
+
+var timer = 100;
+    var intervalID = setInterval(function(){
+        time--
+        console.log(time)
+    }, 1000)
+clearInterval(intervalID)
+
 
 function startQuiz () {
-    console.log("the button was clicked")
     var startScreen = document.getElementById("start-screen");
     startScreen.setAttribute("class", "hide");
     questionsEl.removeAttribute("class");
@@ -31,7 +45,7 @@ function startQuiz () {
 startButton.onclick = startQuiz
 
 function setQuestion() {
-
+    
 
 }
 
